98b1b2f74cd0e463662c45ba0c94ac92c71835f0
Yue Liu <liuyue002@gmail.com>
2021-08-03 14:30:58 +0100

    remove bifurcation related stuff

--- testxpp/plot_solution_branches.m
+++
@@ -1,6 +0,0 @@
-animate_solution_branches(1,7,1);
-animate_solution_branches(1,23:26,2);
-animate_solution_branches(1,42:46,3);
-animate_solution_branches(1,56:60,4);
-animate_solution_branches(2,2:6,5);
-animate_solution_branches(2,24:28,6);
